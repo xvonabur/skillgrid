972eb96112012b7c4f294ca2820b6ddfa1063def
XvonabuR <webdev.xvonabur@gmail.com>
2015-10-28 17:28:16 +0300

    Remove unnecessary field from user factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,6 +5,5 @@ FactoryGirl.define do
     sequence(:email)        { |n| "person_#{n}@example.com" }
     password                'foobar1234'
     password_confirmation   'foobar1234'
-    confirmed_at            Time.now
   end
 end

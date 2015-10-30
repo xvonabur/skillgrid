b265d17fdbc6d00af31704fc5e84df953681d940
XvonabuR <webdev.xvonabur@gmail.com>
2015-10-30 18:10:07 +0300

    Make tests green

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -4,4 +4,33 @@ class Admin::ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      flash[:success] = 'Создан новый продукт'
+      redirect_to admin_products_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    if Product.find(params[:id]).destroy
+      flash[:danger] = 'Продукт успешно удален'
+    else
+      flash[:danger] = 'Не удалось удалить продукт'
+    end
+    redirect_to admin_products_path
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:title, :description)
+    end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,9 @@
 class ProductsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
 end
